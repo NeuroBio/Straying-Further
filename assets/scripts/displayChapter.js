5fac0d9b7d2b9e365b19ca630678dff5719b38b3
kArA <kArARedwing@gmail.com>
2026-01-11 08:05:09 -0500

    add back and forward buttons

--- assets/scripts/displayChapter.js
+++ assets/scripts/displayChapter.js
@@ -1,15 +1,31 @@
 const urlParams = new URLSearchParams(window.location.search);
 const bookId = urlParams.get(QueryParams.BOOK);
-const chapterId = urlParams.get(QueryParams.CHAPTER);
+const chapterId = +urlParams.get(QueryParams.CHAPTER);
 
 const activeBook = Books[bookId];
 
 d3.select(`#book-title`).text(activeBook.title);
 d3.select(`#chapter-title`).text(activeBook.chapters[chapterId - 1].title);
 
+
+
+const priorChapter = d3.select('#prior-chapter');
+if (chapterId === 1) {
+	priorChapter.remove();
+} else {
+	priorChapter.attr(`href`, `./read.html?${QueryParams.BOOK}=${activeBook.id}&${QueryParams.CHAPTER}=${chapterId - 1}`);
+}
+
+const nextChapter = d3.select('#next-chapter');
+if (+chapterId === activeBook.chapters.length) {
+	nextChapter.remove();
+} else {
+	nextChapter.attr(`href`, `./read.html?${QueryParams.BOOK}=${activeBook.id}&${QueryParams.CHAPTER}=${chapterId + 1}`)
+}
+
 fetch(`../assets/chapters/${bookId}/${chapterId}.txt`)
-  .then(response => response.text())
-  .then(content => {
-	d3.select('#chapter-text').text(content);
-  })
-  .catch(error => console.error('Error fetching file:', error));
+	.then(response => response.text())
+	.then(content => {
+		d3.select('#chapter-text').text(content);
+	})
+	.catch(error => console.error('Error fetching file:', error));
