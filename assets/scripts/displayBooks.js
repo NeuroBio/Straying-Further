5db83abeb1d009171418c195e1daa78bce05bceb
kArA <kArARedwing@gmail.com>
2026-01-11 09:25:32 -0500

    spacing might be fixed now

--- assets/scripts/displayBooks.js
+++ assets/scripts/displayBooks.js
@@ -12,7 +12,6 @@ Object.values(Books).forEach((book) => {
 	
 	bookEntry.append('h2')
 		.text(book.title);
-	bookEntry.append('br')
 	bookEntry.append('p')
 		.attr('class', 'short-blurb')
 		.text(book.blurb);
