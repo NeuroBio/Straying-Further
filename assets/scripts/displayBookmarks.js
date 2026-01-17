94f644cb53d57d65a6fdcae669e509d616238c81
kArA <kArARedwing@gmail.com>
2026-01-17 06:47:59 -0500

    show nice thing when no bookmarks

--- assets/scripts/displayBookmarks.js
+++ assets/scripts/displayBookmarks.js
@@ -1,14 +1,14 @@
 const bookmarks = d3.select('#bookmarks');
+let addedBookmark = false;
+
 Object.values(Books).forEach((book) => {
 	let bookmark = localStorage.getItem(book.id);
-	console.log(bookmark);
 	if (!bookmark) {
 		return;
 	}
 	bookmark = JSON.parse(bookmark);
 
 	const bookTitle = book.title;
-	console.log(book.chapters, bookmark)
 	const chapterTitle = book.chapters[bookmark.chapterId - 1].title || bookmark.chapterId;
 	const pageId = +bookmark.pageId;
 	const entry = bookmarks.append('li')
@@ -17,4 +17,10 @@ Object.values(Books).forEach((book) => {
 		.attr('class', 'link-button')
 		.attr('href', `./read.html?${QueryParams.BOOK}=${book.id}&${QueryParams.CHAPTER}=${bookmark.chapterId}&${QueryParams.PAGE}=${pageId}`)
 		.text(`${bookTitle} at chapter: ${chapterTitle}, page ${pageId}`);
+	addedBookmark = true;
 });
+
+console.log(addedBookmark)
+if (!addedBookmark) {
+	bookmarks.append('li').text('No bookmarks found.');
+}
